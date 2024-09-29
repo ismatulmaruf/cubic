3c7ee1ae0ef14d1906d7915f5259b44528be1aef
Md Maruf <maruf509599@gmail.com>
2024-09-29 17:58:01 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 // import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <div className="p-9"></div>
       <ToastContainer />
       <Outlet />
